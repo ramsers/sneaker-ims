0d76492919afe4c6b830fec687c6029318517c4f
Raheim <raheimbbailey@gmail.com>
2020-07-27 15:19:55 -0400

    create sneaker-ims, add routes & index product controller, add menu & header partial

---
+++ app/Controllers/Http/Admin/ProductController.js
@@ -0,0 +1,33 @@
+'use strict'
+
+class ProductController {
+    index({view}) {
+        return view.render('admin/products/all')
+    }
+
+    store() {
+
+    }
+
+    create() {
+
+    }
+
+    show() {
+
+    }
+
+    edit() {
+
+    }
+
+    update() {
+        
+    }
+
+    delete() {
+        
+    }
+}
+
+module.exports = ProductController
