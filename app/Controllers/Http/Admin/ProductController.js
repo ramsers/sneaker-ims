d9ff36fb36a9491fe99c31f5372864ef33769bd5
Raheim <raheimbbailey@gmail.com>
2020-07-28 15:31:52 -0400

    add size to product view field, add store logic to product controller:store

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -1,13 +1,26 @@
 'use strict'
+const Database = use('Database')
 
 class ProductController {
     index({view}) {
         return view.render('admin/products/all')
     }
 
-    store() {
-
-    }
+    async store({request, response}) {
+        try {
+            const post = request.post()
+           await Database.raw(`
+                INSERT INTO products (title, sku, material, description, brand_id, qty, size, user_id)
+                Values('${post.title}', '${post.sku}', '${post.material}', 
+                    '${post.description}', 1, ${post.qty}, 1, 1)
+            `)
+            return `<h1 style ="color: green">Saved Successfully</h1>`
+            } catch (error) {
+                return `<h1 style ="color: red">There was an error</h1>
+                <h3>${error.sqlMessage}</h3>
+                `
+            }
+        }
 
     create({view}) {
         return view.render('admin/products/create')
